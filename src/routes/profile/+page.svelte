6569d2e95080464b44772ff0d43549f93be51eae
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-03-14 21:50:58 +0100

    Add Delete Profile button

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,1 +1,9 @@
-<h1 class="title">Profile Page - TODO</h1>
+<script lang="ts">
+  import { users } from '@stores';
+</script>
+
+<h1 class="title mb-4">Profile Page</h1>
+
+<form>
+  <button type="button" class="btn-primary btn-sm">Delete Profile</button>
+</form>
