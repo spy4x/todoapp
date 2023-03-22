b3804e46162cb6eb08beeaebaba11d8427c62a83
Rajat Sharma <82762243+silver-times@users.noreply.github.com>
2023-03-22 23:40:17 +0100

    Add profile deleted confirmation

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -3,23 +3,47 @@
   import { goto } from '$app/navigation';
 
   let showNotification = false;
+  let profileDeleted = false;
 
   const deleteUserHandler = (id: string) => {
     showNotification = true;
-
     setTimeout(() => {
       showNotification = false;
       auth.deleteUser(id);
+      profileDeleted = true;
     }, 3000);
   };
+
+  $: if (profileDeleted) {
+    setTimeout(() => {
+      profileDeleted = false;
+    }, 3000);
+  }
 </script>
 
 {#if $auth.user}
   <h1 class="title mb-4">Profile Page</h1>
-  <button
-    on:click={deleteUserHandler($auth.user?.id)}
-    class="btn-primary btn-sm">Delete Profile</button
+  <label for="my-modal" class="btn-primary btn-sm px-4 py-2"
+    >Delete Profile</label
   >
+  <input type="checkbox" id="my-modal" class="modal-toggle" />
+  <div class="modal">
+    <div class="modal-box">
+      <h3 class="text-lg font-bold">Delete Profile</h3>
+      <p class="py-4">
+        Are you sure you want to delete your profile? <br />This action cannot
+        be undone.
+      </p>
+      <div class="modal-action">
+        <label
+          on:click={deleteUserHandler($auth.user?.id)}
+          for="my-modal"
+          aria-hidden="true"
+          class="btn">Yes, I'm sure!</label
+        >
+      </div>
+    </div>
+  </div>
 {:else}
   <button on:click={() => goto('/')} class="btn-primary btn-sm"
     >Go to Homepage</button
@@ -28,11 +52,19 @@
 
 {#if showNotification}
   <div class="toast-end toast-bottom toast">
+    <div class="alert alert-info">
+      <div>
+        <span>Deleting {$auth.user?.username}'s profile...</span>
+      </div>
+    </div>
+  </div>
+{/if}
+
+{#if profileDeleted}
+  <div class="toast-end toast-bottom toast">
     <div class="alert alert-success">
       <div>
-        <span
-          >{$auth.user?.username.toUpperCase()}'s profile deleted successfully!</span
-        >
+        <span>Profile deleted successfully!</span>
       </div>
     </div>
   </div>
